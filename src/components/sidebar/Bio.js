b8cba9f00357679cd59d247f9c57c2d87da16343
Romain <romain.roubaix@hotmail.fr>
2021-02-08 22:36:43 +0100

    Enriched about me page

--- src/components/sidebar/Bio.js
+++ src/components/sidebar/Bio.js
@@ -6,7 +6,7 @@ import author_img from '../../images/moi-min.png'
 const Bio = ({ author, tagline }) => {
     return (
         <div className="bio-main w-75">
-            <img src={author_img} style={{ maxWidth: `200px` }} className="profile-img" alt="" />
+            <img src={author_img} style={{ maxWidth: `100px` }} className="profile-img" alt="profile-img" />
             <h3 className="mt-2 author-bio">{author}</h3>
             <small className="text-muted">{tagline}</small>
         </div>
--- src/pages/about.js
+++ src/pages/about.js
@@ -45,35 +45,93 @@ const AboutPage = (props) => {
                         </p>
                         <p>
                             During my second year I had the chance of participating in a sandwich course with the GFI
-                            company.
+                            company (now named <a href="https://gfi.world/fr-en/">Inetum</a>).
                             There I worked in a 15 people team on a huge Java Spring codebase, the clients were
                             University Hospitals everywhere in France.
-                            The app we maintained was used to optimize the process and finances of clinical researches.
+                            The app we maintained was used to optimize the processes and finances of clinical researches.
                         </p>
                         <p>
-                            I continued my studies with a professional bachelor nicknamed DAWIN (Web App Development and
-                            Technological Innovations).
+                            I continued my studies with a professional bachelor nicknamed DAWIN (Web Applications Development and
+                            Technological Innovation).
                             This year was a sandwich course too but this time I worked for a young startup called
-                            DynamicScreen as a fullstack developer.
+                            <a href="https://dynamicscreen.com/"> DynamicScreen</a> as a fullstack developer.
                             DynamicScreen propose a solution to manage fleets of screens displaying informations (digital
                             signage).
                         </p>
                         <p>
                             I am now in a private school called Digital Campus in Bordeaux to obtain a master degree, my
                             formation is nicknamed "Tech Lead", it's the professional role I'm aiming for in the future.
-                            And again... this formation in an apprenticeship, I'm working this year for a communication,
-                            print and web company called AWAM in downtown Bordeaux.
-                            I work there as a full-stack web developer in a small team of 5 very talented colleagues.
+                            And again this formation in an apprenticeship, I'm working this year for a communication,
+                            print and web company called <a href="https://www.agence-awam.fr/">AWAM</a> in downtown Bordeaux.
+                            I work there as a full-stack web developer with a small team of 5 very talented colleagues.
                         </p>
                         <p>
                             I'm a pretty chill yet determined guy and I gather knowledge like a loot hoarder.
                             I strive to become a versatile Web Developer to fulfil the projects entrusted to me.
                         </p>
                         <p>
-                            I put online this blog to share to the world my meager knowledge in the form of articles on
-                            diverse development subjects.
+                            I put online this blog to share to the world my meager knowledge in the form of articles about
+                            diverse development subjects, also to work as a memory of what I used to do.
                         </p>
                         <br/>
+
+                        <h2 className="heading">My personality</h2>
+                        <p>
+                            Everything about my personality can be found following this link :
+                            <a href="https://www.16personalities.com/profiles/ab383cd23b3cc"> Link to 16 personalities</a>
+                        </p>
+                        <p>
+                            TLDR : I'm a ISFJ-T a turbulent defender, a sentinel and my strategy in life should be constant improvement.
+                        </p>
+                        <h2 className="heading">My interests</h2>
+                        <div>
+                             <span className="text-success d-inline-block" title="blazing">
+                                 <FaCheckCircle size={26} style={{color: "success"}}/>
+                             </span>
+                             <p className="d-inline-block ml-3 w-75 align-top">Typing practice</p>
+                             <p>
+                                 In the beginning of 2020 I launched myself on a journey to learn touchtyping (typing fast without looking at the keyboard).
+                                 I spent most of my time practicing on <a href="https://www.typingclub.com/">typing club </a>
+                                 and more recently on <a href="https://www.keybr.com/profile/e3lbx06">keybr</a>
+                             </p>
+                        </div>
+
+                        <div>
+                             <span className="text-success d-inline-block" title="blazing">
+                                 <FaCheckCircle size={26} style={{color: "success"}}/>
+                             </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Competitive programming</p>
+                            <p>
+                                Early 2021 I started working on my problem solving skills.
+                                You can check my progress on Project Euler by adding me as a friend (my friend key : 1770528_pUsdY92K6YVHcr61vRigJbRZVeaJWe5e) and <a href="https://www.codewars.com/users/oui/stats">codeWars</a>
+                            </p>
+                        </div>
+
+                        <div>
+                             <span className="text-success d-inline-block" title="blazing">
+                                 <FaCheckCircle size={26} style={{color: "success"}}/>
+                             </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Game development</p>
+                            <p>
+                               Early 2021 I also began to learn to use <a href="https://godotengine.org/">Godot</a> in order to put down some of my ideas and develop my creativity.
+                            </p>
+                        </div>
+
+                        <div>
+                             <span className="text-success d-inline-block" title="blazing">
+                                 <FaCheckCircle size={26} style={{color: "success"}}/>
+                             </span>
+                            <p className="d-inline-block ml-3 w-75 align-top">Gaming</p>
+                            <p>
+                                I used to play a lot of video games, not so much these days.
+                                I am very fond of independant games made by independant studios.
+                                I play on <a href="https://steamcommunity.com/id/zulutetard">Steam</a>, BattleNet and Nintendo Switch.
+                            </p>
+                        </div>
+
+
+
+
                         {/*<h4>Features</h4>*/}
                         {/*<div>*/}
                         {/*     <span className="text-success d-inline-block" title="blazing">*/}
